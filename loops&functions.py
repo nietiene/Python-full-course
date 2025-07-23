4c947b126839bcf7d8e2a137d47d7395796b40a3
nietiene <niyomugaboetiene53@gmail.com>
2025-07-23 16:14:27 -0700

    commit of python

--- loops&functions.py
+++ loops&functions.py
@@ -5,4 +5,8 @@ while i <= 3:
     while j <= 2:
         print(f"i={i}, j={j}")
         j += 1
-    i += 1    
+    i += 1 
+
+# for eahc time  i will runs also j will runs twice
+# i=1, j=1
+# i=1, j=2    
