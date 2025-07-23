9834e184daf9358014b3c8d4c3fa65c0bcbe8ffb
nietiene <niyomugaboetiene53@gmail.com>
2025-07-23 16:59:43 -0700

    commit of python

--- loops&functions.py
+++ loops&functions.py
@@ -90,4 +90,9 @@ def add_all(*args):
     return total
     
 print(add_all(1, 2, 3, 4))    
+
+def print_info(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key} = {value}")
+print_info(name="Etiene", age=17, school="Gikonko")        
     
