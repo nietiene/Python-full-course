207357c954ef36b12030c35368489c8d915d1428
nietiene <niyomugaboetiene53@gmail.com>
2025-07-24 13:49:38 -0700

    commit of python

---
+++ fileHandling.py
@@ -0,0 +1,6 @@
+# file handling lets your program read from or write to files on your computer
+# common modes r, w, a, r+, b(binary mode)
+
+# Reading a file
+# make sure file exist
+file = open("data.text", "r") 
