51fbd1e0800bec54634352aeb0ab8db642e22ae3
nietiene <niyomugaboetiene53@gmail.com>
2025-07-23 17:28:03 -0700

    commit of python

--- errorHandling.py
+++ errorHandling.py
@@ -3,3 +3,10 @@ try:
     num = int("Hello")
 except:
     print("You must enter a number")    
+
+try:
+    x = 5 / 0
+except: ZeroDivisionError:
+   print("You ca't divide by zero.")
+except: ValueError:
+   print("Wrong value")
