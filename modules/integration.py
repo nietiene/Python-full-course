1a56296a5868ab4ac81f551d9744ffecf07263ce
nietiene <niyomugaboetiene53@gmail.com>
2025-07-24 14:44:37 -0700

    commit of python

--- modules/integration.py
+++ modules/integration.py
@@ -2,3 +2,14 @@ import myOwnModules
 
 print(myOwnModules.add(2,4))
 print(myOwnModules.subtract(4,2))
+
+# import specific function
+
+from myOwnModules import add
+print(myOwnModules.add(4,200))
+
+# Import and assign alias name
+
+import myOwnModules as my
+
+print(my.add(3,1))
