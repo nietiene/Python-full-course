af1835d804140e17c3bca8c64f6b5b465142cbe9
nietiene <niyomugaboetiene53@gmail.com>
2025-07-24 08:16:34 -0700

    commit of python

--- stringManipulation.py
+++ stringManipulation.py
@@ -13,4 +13,5 @@ print(s[0:3]) # start from index 0 up to  index 3
 print(s[2:]) # start from index 2 to the end
 print(s[:4]) # from start or index 0 to index 4
 print(s[::2]) # output: Pto (every 2nd char)
+# start from index 0 and s return every second character
 print(s[::-1]) # Reverse string
