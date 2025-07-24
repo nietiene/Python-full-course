0e4d2fe66352da946cb6c9c017345fcedaafe3fc
nietiene <niyomugaboetiene53@gmail.com>
2025-07-23 17:16:07 -0700

    commit of python

--- loops&functions.py
+++ loops&functions.py
@@ -128,5 +128,6 @@ modif()
 # Lambda funcition (Anonymous function)
 # use only one-line means short function
 # lambda is used to create small, anonymous function (function without name)
+# syntax: lambda arguments: exprression
 square = lambda x: x * x
 print(square(6))
