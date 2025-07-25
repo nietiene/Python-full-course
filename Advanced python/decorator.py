28fb93ea56986b05743f102fd7ed584f01be60b5
nietiene <niyomugaboetiene53@gmail.com>
2025-07-25 16:24:12 -0700

    commit of python

---
+++ Advanced python/decorator.py
@@ -0,0 +1,3 @@
+# decorator is function that takes another fuction as input and returns new function
+# is used when you want to add functionality before ans after function runs
+# used for logging, timing, authentication
