e821eed0d27fa3176d33cfe91b20eb9141d7a527
nietiene <niyomugaboetiene53@gmail.com>
2025-07-25 16:33:44 -0700

    commit of python

--- Advanced python/decorator.py
+++ Advanced python/decorator.py
@@ -1,6 +1,7 @@
 # decorator is function that takes another fuction as input and returns new function
 # is used when you want to add functionality before ans after function runs
 # used for logging, timing, authentication
+# allows to add extra data without changing the function codes
 
 # my_decorator takes func as input
 def my_decorator(func):
