8479d6fba84f84a89dc6628fcb268dbf5eb0e8f1
nietiene <niyomugaboetiene53@gmail.com>
2025-07-26 10:02:09 -0700

    commit of python

--- Advanced python/untTesting.py
+++ Advanced python/untTesting.py
@@ -1,2 +1,15 @@
 # unit testinf is the proccess fo testing small part of code(functions, methods) to make them work correctly
 # ensure code does what it supposed to do
+
+import unittest
+
+def add(x, y):
+    return x * y
+
+class TestMath(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+
+if __name__ == '__main__':
+    unittest.main()
