3dcacdc949fe1b558ad99ae111364349b3eaa57c
nietiene <niyomugaboetiene53@gmail.com>
2025-07-26 09:32:23 -0700

    commit of python

--- Advanced python/exceptionChaining.py
+++ Advanced python/exceptionChaining.py
@@ -1,3 +1,10 @@
 # exception chaining happens when exception is raised while handling another exception
 # it help to track root cause of errors
 # make debugging easier
+
+# default chaining
+
+try:
+    int("abc")
+except ValueError:
+    raise TypeError("Converstion failed")
