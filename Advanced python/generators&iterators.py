a05884621b658deb4d6f10bba827a19e7e335382
nietiene <niyomugaboetiene53@gmail.com>
2025-07-26 07:27:29 -0700

    commit of python

--- Advanced python/generators&iterators.py
+++ Advanced python/generators&iterators.py
@@ -1,1 +1,9 @@
 # iterator lets you loop through elements one at one using next
+
+my_list = [10, 20, 30]
+# iter() turns the list into iterator object
+it = iter(my_list)
+
+print(next(it)) # gets the first value
+print(next(it)) # next one
+print(next(it)) # also next one
