485dcb7b145f887d539e56df89df88797bf3cc26
nietiene <niyomugaboetiene53@gmail.com>
2025-07-26 09:24:57 -0700

    commit of python

--- Advanced python/contextManager.py
+++ Advanced python/contextManager.py
@@ -1,14 +1,18 @@
 # contex manager is tool that maanes setip and cleanup actions automatically
 # is mosttyl used in filejandling, db connections, locks..
 # with handles file opening and closing automatically
-with open("notext.nxt", "r") as f:
+with open("notes.txt", "r") as f:
     data = f.read()
     print(data)
 
 
-class myCntext:
+class myContext:
     def __enter__(self):
         print("==> Entring block")
         return "resource"
     
-    def __exit__(self, exc_type, exc_value, traceback)
+    def __exit__(self, exc_type, exc_value, traceback):
+        print("==> Existing block")
+
+with myContext() as res:
+    print("Using:", res)
