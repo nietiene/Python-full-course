8dee9af64afbe67f1743c6c8275fe139611490f8
nietiene <niyomugaboetiene53@gmail.com>
2025-07-26 09:19:48 -0700

    commit of python

--- Advanced python/contextManager.py
+++ Advanced python/contextManager.py
@@ -1,7 +1,6 @@
 # contex manager is tool that maanes setip and cleanup actions automatically
 # is mosttyl used in filejandling, db connections, locks..
-
+# with handles file opening and closing automatically
 with open("notext.nxt", "r") as f:
     data = f.read()
     print(data)
-    
