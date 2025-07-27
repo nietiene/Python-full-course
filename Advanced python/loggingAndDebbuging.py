91e5ddef14126ca4d1ad76432330fec08f9194b5
nietiene <niyomugaboetiene53@gmail.com>
2025-07-27 15:12:52 -0700

    commit of python

--- Advanced python/loggingAndDebbuging.py
+++ Advanced python/loggingAndDebbuging.py
@@ -1,2 +1,8 @@
 # logging is the proces of recording events, error, warings or info during the program execution
 # so instead of using print we use logging instead of print
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+logging.debug("This is debug message")
