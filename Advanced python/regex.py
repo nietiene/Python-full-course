0c7ef84b29df19808334fb3d1a14cdf259224c60
nietiene <niyomugaboetiene53@gmail.com>
2025-07-27 16:37:27 -0700

    commit of python

--- Advanced python/regex.py
+++ Advanced python/regex.py
@@ -5,5 +5,13 @@
    re.search() -> search for anywhere in string 
    re.match() -> matches from beginning of string
    re.findall()  -> return all matches as list
+   re.sub() -> replace pattern with something else
 """
+
 import re
+
+text = "My email is example@gmail.com"
+
+match = re.search(r'/w+/w+@/w+w+.', text)
+if match:
+    print("Found", match.group())
