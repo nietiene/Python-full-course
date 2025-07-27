e8d8238891ee8c28005aea90db2f458cc207aedc
nietiene <niyomugaboetiene53@gmail.com>
2025-07-27 16:33:53 -0700

    commit of python

--- Advanced python/regex.py
+++ Advanced python/regex.py
@@ -1,1 +1,8 @@
 # Regex is way of  of search, match, and manipulate strings using patterns
+
+""""
+   ////Basic Regex functions/////
+   re.search() -> search for anywhere in string 
+   re.match() ->
+"""
+import re
