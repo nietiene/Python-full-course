2d5c7ba5b07536dbd1e6135a4b3f60d8f82fdb20
nietiene <niyomugaboetiene53@gmail.com>
2025-07-28 13:31:29 -0700

    commit of python

--- flask/touting&templates.py
+++ flask/touting&templates.py
@@ -1,1 +1,10 @@
 # routing means mapping url to a python function when user visits URL flask runs the fucntion and return something like htmpl pages
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def home():
+    return "Welcome to homepage"
