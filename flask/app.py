a1ae39932e6d59ad42101053bcf78818b4fc188f
nietiene <niyomugaboetiene53@gmail.com>
2025-07-28 10:44:11 -0700

    commit of python

--- flask/app.py
+++ flask/app.py
@@ -1,11 +1,14 @@
 # flask is web framework used to build web application
 # it can handle both frontend and backend
 # it when you're using any frontend framework you can use it for backend only 
-from flask import Flask, render_template
+
+from flask import Flask, render_template 
+# in this code flask is mai
 
 app = Flask(__name__)
 
 @app.route('/')
+
 def home():
     return render_template("home.html")
 
