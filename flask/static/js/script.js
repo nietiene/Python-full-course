87756c1af9340aae8254e318ca48fbdb5956abdb
nietiene <niyomugaboetiene53@gmail.com>
2025-07-29 15:14:35 -0700

    commit of python

--- flask/static/js/script.js
+++ flask/static/js/script.js
@@ -2,5 +2,7 @@ function updateName() {
     const input = document.getElementById("nameInput").value;
     const span = document.getElementById("username")
 
-    if (input.trim() === "") 
+    if (input.trim() === "") {
+        alert("Please enter a name.")
+    }
 }
