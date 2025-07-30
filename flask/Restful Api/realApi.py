6dac93ce2b430f1ee6f741e5932dbd72063a1dea
nietiene <niyomugaboetiene53@gmail.com>
2025-07-30 14:23:38 -0700

    commit of python

--- flask/Restful Api/realApi.py
+++ flask/Restful Api/realApi.py
@@ -1,2 +1,15 @@
 from flask import Flask, jsonify, request
 import pymysql
+
+app = Flask(__name__)
+
+# database connection
+
+def get_connection():
+    return pymysql.connect(
+        host='localhost',
+        user='root',
+        password='',
+        db='student_api_py',
+        cursorclass=pymysql.cursors.DictCursor
+    )
