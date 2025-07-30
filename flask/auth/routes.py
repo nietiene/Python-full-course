e3991faf40a1bc921ceb1a49b48d9ec324269f3b
nietiene <niyomugaboetiene53@gmail.com>
2025-07-30 08:02:59 -0700

    commit of python

--- flask/auth/routes.py
+++ flask/auth/routes.py
@@ -3,3 +3,11 @@ from flask import Blueprint, render_template
 # create blueprint
 auth = Blueprint('auth', __name__)
 
+# define routes
+@auth.route('/login')
+def login():
+    return "<h2>Login page</h2>"
+
+@auth.route('/register')
+def register():
+    return "<h2>Register Page</h2>"
