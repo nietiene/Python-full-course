918b4d2458b8f48e51d512f34f3ba97a9bfb0500
nietiene <niyomugaboetiene53@gmail.com>
2025-07-30 08:15:28 -0700

    commit of python

--- flask/auth/routes.py
+++ flask/auth/routes.py
@@ -2,6 +2,8 @@ from flask import Blueprint
 
 # create blueprint
 auth = Blueprint('auth', __name__)
+# it creates blueprint named auth
+# and then __name__ is help flask to know file location
 
 # define routes
 @auth.route('/login')
