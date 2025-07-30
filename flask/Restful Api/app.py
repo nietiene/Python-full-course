605d15659e6ffa5773fa5b3efe206d0d94a338d0
nietiene <niyomugaboetiene53@gmail.com>
2025-07-30 08:30:21 -0700

    commit of python

--- flask/Restful Api/app.py
+++ flask/Restful Api/app.py
@@ -3,3 +3,11 @@ from flask_restful import Api
 
 app = Flask(__name__)
 api = Api(app)
+
+# fake database for study
+
+sutdents = {
+    1: {"name": "Etiene", "Age": 17},
+    2: {"name": "Alice", "Age": 22},
+
+}
